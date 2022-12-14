50eb258c090a1369c0f96c15ec953c1765ceae99
ola <O.Melnichuk@kdd-digital.de>
2022-12-14 22:52:51 +0100

    add book-item component; add level type enum

---
+++ e-book/src/app/enums/level.enum.ts
@@ -0,0 +1,4 @@
+export enum LevelEnum {
+  BEGINNER = "BEGINNER",
+  ADVANCED = "ADVANCED"
+}
---
+++ e-book/src/app/pages/home/components/book-item/book-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from "@angular/core";
+import { Book } from "../../../../models/book";
+
+@Component({
+  selector: 'app-book-item',
+  templateUrl: './book-item.component.html',
+  styleUrls: ['./book-item.component.scss']
+})
+export class BookItemComponent {
+  @Input() book: Book;
+
+}
